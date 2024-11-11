0d4f7184013ec972cbd25bbaa5073c1d5b164a75
Øyvind Bleka <oyvble@hotmail.com>
2024-11-11 13:40:48 +0100

    make installation work

--- src/EM_estLoop.cpp
+++ src/EM_estLoop.cpp
@@ -3,18 +3,15 @@
 using namespace arma;
 
 extern "C" {
-//[[Rcpp::depends(RcppArmadillo)]]
-//[[Rcpp::export]]
  
 void EM_estLoop(double *xptr, double *lambda, double *mu, double *sigmaSq, double *p1, int *n, double *epsilon) {
-  //Rcpp::NumericVector retvec(4); //init return vector
   static const double pi = 3.14159265;
   
   //copy variables
-  vec *x = new vec(xptr, *n, false);
-  vec x2 = (*x)%(*x);  //Calc elementwise/Schur product and keeping long vector
+  vec xvec(xptr, *n, false); //init vector
+  vec xvecsq = xvec%xvec;  //Calc elementwise/Schur product and keeping long vector
   vec z; 
-  long double sumX = sum(*x);
+  long double sumX = sum(xvec);
   long double con,mu2,sumZ,sumXZ,sum1Z; //help variables
   long double lambda_new,mu_new,sigmaSq_new,p1_new; //declar variables
   double log2pi = log(2*pi); //obtain constant
@@ -22,18 +19,18 @@ void EM_estLoop(double *xptr, double *lambda, double *mu, double *sigmaSq, doubl
   while (diff) { 
     mu2 = (*mu)*(*mu); //mu^2
     con = 0.5*(mu2/ *sigmaSq + log(*sigmaSq) + log2pi) + log(*lambda); //constant to update
-    z = -0.5*x2/ *sigmaSq + (*x)*( *mu / *sigmaSq + *lambda) - con; //calulcate logged expression
+    z = -0.5*xvecsq/ *sigmaSq + xvec*( *mu / *sigmaSq + *lambda) - con; //calulcate logged expression
     z = *p1/(*p1+(1-*p1)*exp(z));
     
     sumZ = sum(z);
-    sumXZ = sum((*x)%z);
+    sumXZ = sum(xvec%z);
     sum1Z = (*n - sumZ); //sum(1-z);
     
     p1_new = sumZ/ *n;
     lambda_new = sumZ/sumXZ;
     mu_new = (sumX - sumXZ)/sum1Z; //sum((1-z)*x)/sum(1-z);
     
-    vec tmp = *x-mu_new;
+    vec tmp = xvec-mu_new;
     sigmaSq_new = sum( tmp%tmp%(1-z) )/sum1Z;
     
     //diff <- !(all(c(abs(lambda_new-*lambda),abs(mu_new-*mu),abs(sigmaSq_new-*sigmaSq),abs(p1_new-*p1))<epsilon))
@@ -45,7 +42,6 @@ void EM_estLoop(double *xptr, double *lambda, double *mu, double *sigmaSq, doubl
     *sigmaSq = sigmaSq_new; 
     *p1 = p1_new;
   }
-   delete x;
 }
 
 } //end external
